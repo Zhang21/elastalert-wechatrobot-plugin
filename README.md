456f4365f3f25ddefb4d6d95c420e1daac23d83d
zhang <zhang@centos7.vmware>
2020-04-30 16:03:27 +0800

    add wechat_robot_plugin

---
+++ README.md
@@ -0,0 +1,13 @@
+# elastalert-wechatrobot-plugin
+
+参考:
+
+- elastalert-dingtalk-plugin: <https://github.com/xuyaoqiang/elastalert-dingtalk-plugin>
+
+<br>
+
+ElastAlert企业微信群组机器人告警通知。和钉钉机器人那个一样。只是两者webhook传参不一样。
+
+完全是照着elastalert-dingtalk-plugin插件修改的，因为企业微信机器人webhook里的某几个参数与钉钉机器人webhook不一样，所以需要改一下参数，代码是一样的。
+
+使用方法请参考elastalert-dingtalk-plugin。
---
+++ elastalert_modules/wechat_robot.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+
+"""A plugin of ElastAlert for inotify to wechat group robot.
+
+@reference: https://github.com/xuyaoqiang/elastalert-dingtalk-plugin
+@wechat_robot: https://work.weixin.qq.com/help?doc_id=13376
+@date: 2020-04-30
+@python: v3.6
+@license: MIT
+"""
+
+
+import json
+import requests
+from elastalert.alerts import Alerter, DateTimeEncoder
+from requests.exceptions import RequestException
+from elastalert.util import EAException
+
+
+class WechatRobotAlerter(Alerter):
+    retuired_options = frozenset(['wechat_robot_webhook', 'wechat_robot_msgtype'])
+
+    def __init__(self, rule):
+        super(WechatRobotAlerter, self).__init__(rule)
+        self.wechat_robot_webhook = self.rule['wechat_robot_webhook']
+        self.wechat_robot_msgtype = self.rule.get('wechat_robot_msgtype', 'text')
+        self.wechat_robot_mentioned_list = self.rule.get('wechat_robot_mentioned_list', [])
+
+    def format_body(self, body):
+        return body.encode('utf8')
+
+    def alert(self, matches):
+        headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/json;charset=utf-8"
+        }
+        body = self.create_alert_body(matches)
+        payload = {
+            "msgtype": self.wechat_robot_msgtype,
+            "text": {
+                "content": body,
+                "mentioned_list": self.wechat_robot_mentioned_list
+            }
+        }
+        try:
+            response = requests.post(self.wechat_robot_webhook,
+                                     data=json.dumps(payload, cls=DateTimeEncoder),
+                                     headers=headers)
+            response.raise_for_status()
+        except RequestException as e:
+            raise EAException("Error request to wechat_robot: {0}".format(str(e)))
+
+    def get_info(self):
+        return {
+            "type": "wechat_robot",
+            "wechat_robot_webhook": self.wechat_robot_webhook
+        }
+        pass
